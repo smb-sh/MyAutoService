41e9b3c2da62f9146be945eca4291e0fd275ee01
smb-sh <smbsh20@gmail.com>
2022-08-09 21:39:25 +0430

    tagHelper model & directory

---
+++ MyAutoService/Models/PagingInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAutoService.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPage => (int)Math.Ceiling((decimal)TotalItems / ItemPerPage);
+        public string UrlParam { get; set; }
+    }
+}
---
+++ MyAutoService/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using MyAutoService.Models;
+
+namespace MyAutoService.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string PageAction { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+    }
+}
