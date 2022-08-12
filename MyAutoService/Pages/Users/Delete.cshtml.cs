5dc45b544571fa42d318d8a2f8e9262dd364d32d
smb-sh <smbsh20@gmail.com>
2022-08-12 23:40:29 +0430

    Add Delete rasor page for user

---
+++ MyAutoService/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyAutoService.Data;
+using MyAutoService.Models;
+
+namespace MyAutoService.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DeleteModel(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [BindProperty] public ApplicationUser ApplicationUser { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+            if (id.Trim().Length == 0)
+                return NotFound();
+
+            ApplicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (ApplicationUser == null)
+                return NotFound();
+
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUser = await _db.ApplicationUsers.FindAsync(id);
+
+            if (ApplicationUser != null)
+            {
+                _db.ApplicationUsers.Remove(ApplicationUser);
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
